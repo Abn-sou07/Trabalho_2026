e93afcfdfa1e066756566dcb144beef50bedb175
Mmessiaspm <mmessias@ifpi.edu.br>
2026-06-11 08:17:31 -0300

    Aula 11/06/2026

---
+++ ImprimiIdades.java
@@ -0,0 +1,12 @@
+public class ImprimiIdades {
+    public static void main(String[] args) {
+        int[] idades = new int [10];
+        for (int i = 0; i < idades.length; i++) {
+                idades[i] = i * 10;         
+        }
+        for (int i : idades) {
+            System.out.println("Idade " + i);
+        }
+    }
+    
+}
