ac0e33525913adaf1b41eb341c4658ede998c9f7
Mmessiaspm <mmessias@ifpi.edu.br>
2026-06-11 08:51:00 -0300

    classe scanner 11/06/2026

---
+++ ExemploScannerConsole.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class ExemploScannerConsole {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Digite seu nome:");
+        String nome = scanner.nextLine();
+        System.out.println("Digite sua idade:");
+        int idade = scanner.nextInt();
+        System.out.println("Olá: " + nome + 
+        " Você tem: "  + idade + " Anos");
+        scanner.close();
+    }
+
+}
